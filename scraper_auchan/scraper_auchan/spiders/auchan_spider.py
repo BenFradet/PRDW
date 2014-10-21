1dfe1cf70428f8e701147bea324d5f1031061b84
BenFradet <benjamin.fradet@gmail.com>
2014-10-21 23:34:52 +0200

    new crawling spider

--- scraper_auchan/scraper_auchan/spiders/auchan_spider.py
+++ scraper_auchan/scraper_auchan/spiders/auchan_spider.py
@@ -1,16 +1,23 @@
 import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
 
 from scraper_auchan.items import ScraperAuchanItem
 
-class AuchanSpider(scrapy.Spider):
+class AuchanSpider(CrawlSpider):
     name = 'auchan'
     allowed_domains = ['www.auchandirect.fr']
-    start_urls = [
-            'http://www.auchandirect.fr/boissons/eaux/eau-de-source/id1/478/' +
-            '4607'
-            ]
+    start_urls = ['http://www.auchandirect.fr/marche/legumes/id3/404/463']
 
-    def parse(self, response):
+    followRegex = 'http://www.auchandirect.fr/[a-z]+/[a-z]+/[a-z-,]+/id0/[0-9]+'
+    parseRegex = 'http://www.auchandirect.fr/[a-z]+/[a-z]+/[a-z-]+/id1/' + \
+        '[0-9]+/[0-9]+'
+    rules = (
+            Rule(LinkExtractor(allow=(followRegex, ))),
+            Rule(LinkExtractor(allow=(parseRegex, )), callback='parse_product'),
+            )
+
+    def parse_product(self, response):
         for sel in response.xpath('//div[@class="fiche-produit"]'):
             item = ScraperAuchanItem()
             item['id'] = response.url.split('/')[-1]
