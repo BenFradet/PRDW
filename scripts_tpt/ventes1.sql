36809e3adc2a309ce6fc92a9d89276f335be2efc
BenFradet <benjamin.fradet@gmail.com>
2014-12-04 14:51:31 +0100

    create ventes1 table script

---
+++ scripts_tpt/ventes1.sql
@@ -0,0 +1,16 @@
+create table ventes1 (
+    LocID       varchar(30),
+    TPVID       varchar(30),
+    TkNum       varchar(30),
+    TkDT        varchar(30),
+    TkHR        varchar(30),
+    VisID       varchar(30),
+    VisTypeCD   varchar(30),
+    CliID       varchar(30),
+    CliStatCD   varchar(30),
+    TklNetAmt   varchar(30),
+    Qty         varchar(30),
+    Mesure      varchar(30),
+    ItemID      varchar(30),
+    RemAmt      varchar(30)
+);
