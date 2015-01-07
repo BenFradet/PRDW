1192a1e39292c7d3ef4aec9c0005876bf0c68343
BenFradet <benjamin.fradet@gmail.com>
2015-01-07 20:28:22 +0100

    script to load familles csv

---
+++ scripts_tpt/csvToStg/createFamille.sql
@@ -0,0 +1,15 @@
+create table db_prdw_stg.famille (
+    LOCID       varchar(50),
+    TPVID       varchar(50),
+    TKNUM       varchar(50),
+    TKDT        varchar(50),
+    TKHR        varchar(50),
+    PAYMCD      varchar(50),
+    ACCTNUM     varchar(50),
+    TKNETAMT    varchar(50),
+    CLIID       varchar(50),
+    CLISTATCD   varchar(50),
+    VISID       varchar(50),
+    VISTYPECD   varchar(50),
+    CtrlCli     varchar(50)
+);
